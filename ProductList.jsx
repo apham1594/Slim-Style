2e77b66ce7d5f394d1a86fbe6d5fae35b9ec112d
Anthony Pham <88216187+apham1594@users.noreply.github.com>
2022-02-21 18:10:00 -0700

    Add files via upload

---
+++ ProductList.jsx
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+import { mobile } from "../responsive";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+  ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })}
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+  ${mobile({ marginRight: "0px" })}
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+  ${mobile({ margin: "10px 0px" })}
+`;
+const Option = styled.option``;
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Title>Services </Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter :</FilterText>
+          <Select>
+            <Option disabled selected>
+              Color
+            </Option>
+            <Option> Slim Cuts</Option>
+            <Option>Color</Option>
+            <Option>Slim Braid</Option>
+            <Option>Blue</Option>
+            <Option>Styles</Option>
+            <Option>Twist</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Color of Hair/Optional
+            </Option>
+            <Option>Blonde</Option>
+            <Option>Black</Option>
+            <Option>Dirty Blonde</Option>
+            <Option> Purple</Option>
+            <Option>White</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ProductList;
